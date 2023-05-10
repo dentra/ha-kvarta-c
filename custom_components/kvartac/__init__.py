aa19fa0d036e414d504e01a81030f8c48115018d
dentra <dennis.trachuk@gmail.com>
2023-05-10 14:33:50 +0000

    replace deprecated async_setup_platforms

--- custom_components/kvartac/__init__.py
+++ custom_components/kvartac/__init__.py
@@ -68,7 +68,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if not entry.update_listeners:
         entry.add_update_listener(async_update_options)
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
